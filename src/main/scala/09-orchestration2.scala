46fdd221a488d30289319bd209dd057142165b55
Dave Gurnell <dave@untyped.com>
2016-01-19 21:18:43 +0000

    Exercises 09 and 10

--- src/main/scala/09-orchestration2.scala
+++ src/main/scala/09-orchestration2.scala
@@ -1,56 +1,45 @@
+import scala.concurrent.Await
+import scala.concurrent.duration._
+import scala.concurrent.ExecutionContext.Implicits._
+import scala.concurrent.Future
 import scala.language.higherKinds
+import scalaz.Id._
 import scalaz.Monad
-import scalaz.Monoid
+import scalaz.std.scalaFuture._
+import scalaz.syntax.monad._
 
 object OrchestrationExample2 {
   final case class Avatar(url: String)
 
-  sealed trait AvatarService[F[_]] {
+  // TODO:
+  //  - Modify RealAvatarService so that:
+  //     - getAvatarUrl fails if the email doesn't contain "dave"
+  //     - getAvatar fails if the url doesn't contain "example.com"
+
+  trait AvatarService[F[_]] {
     implicit def monad: Monad[F]
 
     def getAvatarUrl(email: String): F[String]
 
     def getAvatar(url: String): F[Avatar]
 
-    final def getAvatarForEmail(email: String): F[Avatar] = {
-      import scalaz.syntax.monad._
-
-      for {
-        person <- getAvatarUrl(email)
-        avatar <- getAvatar(person)
-      } yield avatar
-    }
+    final def getAvatarForEmail(email: String): F[Avatar] = for {
+      person <- getAvatarUrl(email)
+      avatar <- getAvatar(person)
+    } yield avatar
   }
 
-  import scalaz.\/
-  import scalaz.EitherT
-  import scala.concurrent.Future
-  type FutureFallable[A] = EitherT[Future, String, A]
+  object RealAvatarService extends AvatarService[Future] {
+    val monad = Monad[Future]
 
-  import scalaz.syntax.either._
-  import scalaz.std.scalaFuture._
-  import scala.concurrent.ExecutionContext.Implicits._
-  final case object RealAvatarService extends AvatarService[FutureFallable] {
-    val monad = Monad[FutureFallable]
+    def getAvatarUrl(email: String): Future[String] =
+      Future.successful(s"http://avatarstuff.com/$email")
 
-    def getAvatarUrl(email: String): FutureFallable[String] = {
-      if(email contains "example.com") {
-        EitherT(Future.successful(s"http://avatarstuff.com/$email".right))
-      } else {
-        EitherT(Future.successful("Email not found".left))
-      }
-    }
-
-    def getAvatar(url: String): FutureFallable[Avatar] =
-      if(url contains "dave") {
-        EitherT(Future.successful(Avatar(url).right))
-      } else {
-        EitherT(Future.successful("Avatar not found".left))
-      }
+    def getAvatar(url: String): Future[Avatar] =
+      Future.successful(Avatar(url))
   }
 
-  import scalaz.Id._
-  final case object FakeAvatarService extends AvatarService[Id] {
+  object FakeAvatarService extends AvatarService[Id] {
     val monad = Monad[Id]
 
     def getAvatarUrl(email: String): String =
@@ -61,9 +50,7 @@ object OrchestrationExample2 {
   }
 
   def main(args: Array[String]) = {
-    import scala.concurrent.Await
-    import scala.concurrent.duration._
-    println(Await.result(RealAvatarService.getAvatarForEmail("dave@example.com").run, 3.seconds))
+    println(Await.result(RealAvatarService.getAvatarForEmail("dave@example.com"), 3.seconds))
 
     println(FakeAvatarService.getAvatarForEmail("dave@example.com"))
   }
--- src/main/scala/10-applicative.scala
+++ src/main/scala/10-applicative.scala
@@ -3,34 +3,29 @@ import scala.concurrent.Future
 import scala.concurrent.Await
 import scala.concurrent.ExecutionContext.Implicits._
 import scala.concurrent.duration._
-import scalaz.Applicative
 import scalaz.std.scalaFuture._
-import scalaz.syntax.applicative._
 
 object ApplicativeExample {
+  // TODO:
+  //  - Implement parallelTotal using applicative syntax
+  //  - Check it runs faster than sequentialTotal
 
-  def getTraffic(hostname: String): Future[Int] =
-    Future {
-      Thread.sleep(1000)
-      (hostname.## % 1000).toInt
-    }
-
-  def main(args: Array[String]) = {
-    val sequentialTotal = for {
-      a <- getTraffic("host1")
-      b <- getTraffic("host2")
-      c <- getTraffic("host3")
-      d <- getTraffic("host4")
-    } yield a + b + c + d
-
-    val parallelTotal = (
-      getTraffic("host1") |@|
-      getTraffic("host2") |@|
-      getTraffic("host3") |@|
-      getTraffic("host4")
-    )(_ + _ + _ + _)
-
-    println(Await.result(parallelTotal, 2.seconds))
-    println(Await.result(sequentialTotal, 2.seconds))
+  def getTraffic(hostname: String): Future[Int] = Future {
+    Thread.sleep(1000)
+    (hostname.## % 1000).toInt
   }
+
+  val sequentialTotal: Future[Int] = for {
+    a <- getTraffic("host1")
+    b <- getTraffic("host2")
+    c <- getTraffic("host3")
+    d <- getTraffic("host4")
+  } yield a + b + c + d
+
+  val parallelTotal: Future[Int] = ???
+
+  println(Await.result(parallelTotal, 2.seconds))
+  println(Await.result(sequentialTotal, 2.seconds))
+
+  def main(args: Array[String]): Unit = ()
 }
