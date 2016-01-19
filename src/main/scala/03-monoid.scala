a66e049da3077f033d1c377d9d522891b5381f41
Dave Gurnell <dave@untyped.com>
2016-01-19 20:42:43 +0000

    Exercise 03

--- src/main/scala/03-monoid.scala
+++ src/main/scala/03-monoid.scala
@@ -1,11 +1,18 @@
 import scalaz.Monoid
 
-object SuperAdder {
-  // Adding Ints directly:
+object MonoidExample {
+  // TODO:
+  //  - Complete the implementation of `add1`,
+  //    which sums a List of Ints without using Monoids:
   def add1(items: List[Int]): Int =
-    items.foldLeft(0){ _ + _ }
+    ???
 
-  // Adding Ints using Monoids:
+  println(add1(List(1, 2, 3, 4, 5)))
+
+  // TODO:
+  //  - Complete the implementation of `add2`.
+  //    This time, summon a Monoid for Int
+  //    and use Monoid syntax to sum the list
   def add2(items: List[Int]): Int = {
     import scalaz.std.anyVal._
     import scalaz.syntax.monoid._
@@ -13,30 +20,27 @@ object SuperAdder {
     items.foldLeft(mzero[Int]){ _ |+| _ }
   }
 
-  // Adding anything that has a Monoid:
-  def add3[A](items: List[A])(implicit monoid: Monoid[A]): A = {
-    import scalaz.syntax.monoid._
+  println(add2(List(1, 2, 3, 4, 5)))
 
-    items.foldLeft(mzero){ _ |+| _ }
-  }
-}
+  // TODO:
+  //  - Write an `add3` method that takes two parameters:
+  //     - a List[A]
+  //     - an implicit Monoid[A]
+  //  - Use the monoid to sum the list
 
-object MonoidExample {
-  def main(args: Array[String]) = {
-    println(SuperAdder.add1(List(1, 2, 3, 4, 5)))
+  // TODO:
+  //  - Use `add3` to sum the following lists.
+  //    You may need to import certain instances of Monoid
+  //    to make your code compile:
 
-    println(SuperAdder.add2(List(1, 2, 3, 4, 5)))
+  // println(add3(List(1, 2, 3, 4, 5)))
+  // println(add3(List("1", "2", "3", "4", "5")))
+  // println(add3(List(None, Some(1), Some(2), Some(3), Some(4), Some(5))))
 
-    import scalaz.std.anyVal._
-    println(SuperAdder.add3(List(1, 2, 3, 4, 5)))
+  // TODO:
+  //  - Use `add3` to *multiply together* the following list of ints.
+  //    You'll need to select the multiplication Monoid
+  //    instead of the default addition monoid
 
-    import scalaz.std.string._
-    println(SuperAdder.add3(List("1", "2", "3", "4", "5")))
-
-    import scalaz.std.option._
-    println(SuperAdder.add3(List(None, Some(1), Some(2), Some(3), Some(4), Some(5))))
-
-    import scalaz.Tags.Multiplication
-    println(SuperAdder.add3(List(1, 2, 3, 4, 5).map(Multiplication.apply)))
-  }
+  // println(add3(List(1, 2, 3, 4, 5)))
 }
